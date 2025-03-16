5f2d304f7b1177e6717feafde4acab22ae62d20d
mumeimumei00 <mumeimumei0@gmail.com>
2025-03-16 22:35:32 +0900

    Turned All Integration method into classes that inherit from a parent class called Integration

--- Integration/Integration.py
+++ Integration/Integration.py
@@ -41,16 +41,59 @@ class RectangularRuleRight(Integral):
         return h*np.sum(self.funct(np.linspace(self.bound[0]+h,self.bound[1],int((self.bound[1]-self.bound[0])/h))))
 
 
+class RectangularRuleLeft(Integral):
+    def __init__(self,funct,bound,n):
+        super().__init__(funct, bound, n)
+        self.name = "Left Rectangular Rule"
+
+    def integrate(self):
+        h = (self.bound[1]-self.bound[0])/self.n
+        return h*np.sum(self.funct(np.linspace(self.bound[0],self.bound[1]-h,int((self.bound[1]-self.bound[0])/h))))
+
+class RectangularRuleMix(Integral):
+    def __init__(self,funct,bound,n):
+        super().__init__(funct, bound, n)
+        self.name = "Mix Rectangular Rule"
+
+    # def integrate(self):
+    #     return (RectangularRuleLeft(self.funct, n= self.n/2) + RectangularRuleRight(self.funct, n= self.n/2))/2
+
+class TrapezoidalRule(Integral):
+    def __init__(self,funct,bound,n):
+        super().__init__(funct, bound, n)
+        self.name = "Trapezoidal Rule Rule"
+
+    def integrate(self):
+        h = (self.bound[1]-self.bound[0])/self.n
+        return (h/2)*( 2*np.sum(self.funct(np.linspace(self.bound[0]+h,self.bound[1]-h,int((self.bound[1]-self.bound[0])/h)-2)))+ self.funct(self.bound[0]) + self.funct(self.bound[1]) ) 
 
 
+class MonteCarlo(Integral):
+    def __init__(self,funct,bound,n):
+        super().__init__(funct, bound, n)
+        self.name = "Monte Carlo Method"
 
-# def RectangularRuleRight(funct,bound = [0,5],n=50000):
-#     h = (bound[1]-bound[0])/n
-#     return h*np.sum(funct(np.linspace(bound[0]+h,bound[1],int((bound[1]-bound[0])/h))))
-#
+    def integrate(self):
+        random_number= np.random.uniform(self.bound[0],self.bound[1],self.n)
+        return np.mean(self.funct(random_number))*(self.bound[1]-self.bound[0])
 
-integral = iter(RectangularRuleRight(function,[0,5],2))
+
+class SimpsonRule(Integral):
+    def __init__(self,funct,bound,n):
+        super().__init__(funct, bound, n)
+        self.name = "SimpsonRule Rule"
+
+    def integrate(self):
+        h = (self.bound[1]-self.bound[0])/self.n
+        boundterm= self.funct(self.bound[0])+self.funct(self.bound[1])
+        oddterm = np.sum(4*self.funct(h*np.array([self.bound[0]+(2*x+1) for x in range(int(self.n/2))])))
+        eventerm = np.sum(2*self.funct(h*np.array([self.bound[0]+(2*x) for x in range(1,int(self.n/2))])))
+        return (h/3)*(boundterm+oddterm+eventerm)
+
+integral = iter(SimpsonRule(function,[0,5],2))
 # print(integral.integrate())
 # gprint(integral)
 for x in range (100):
     print(next(integral))
+
+
--- README.md
+++ README.md
@@ -11,6 +11,6 @@ This is a repo where I experiment with a bunch of scientific tools
 - [X] Integral Simpson method
 - [X] Test Unit
 - [ ] Change the structure of Files
-- [ ] Use Inheritance to create a class Integral
+- [X] Use Inheritance to create a class Integral
 - [ ] Visualize accuracy versus n
 - [X] Real Performance Test
