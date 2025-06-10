992ddcc9c0e801ce58c5fdf8e8f9fab04f7e8673
mumeimumei00 <mumeimumei0@gmail.com>
2025-06-10 19:46:35 +0900

    Eperimenting with RSA

---
+++ DataStructureAndAlgorithm/RSA.py
@@ -0,0 +1,33 @@
+import sympy as sy
+import random
+
+def primegenerator():
+    exitcondition = True;
+    while (exitcondition):
+        p = random.randint(0,2097152)
+        q = random.randint(0,2097152)
+        if (sy.isprime(p) and sy.isprime(q) and p!=q):
+            exitcondition = False
+
+
+    return p,q
+
+
+m = 8838
+
+def RSA(m):
+    p,q = primegenerator()
+    n = p*q
+    r = (p-1)*(q-1)
+    e = 65537
+
+    s,t,g = sy.gcdex(e,r)
+    d = s%r
+    return pow(m,e,n)
+
+encry = RSA(m)
+print("this is the encrypted message: ", encry)
+
+dec = pow(encry,int(d,n)
+print("\nThis is the decrypted message: ", dec)
+
